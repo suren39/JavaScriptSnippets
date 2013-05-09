6849c0e7e19fe3899ca60e23b44916304bac57a6
Suren Abrahamyan <suren39@gmail.com>
2013-05-09 13:58:00 -0700

    Added globalScript and HTML template
    HTML template includes an eBay version which has a safe JS import.

---
+++ Ajax/yahooStoreAjaxCart.js
@@ -0,0 +1,25 @@
+	/*var action = "http://order.store.yahoo.net/botach/cgi-bin/wg-order?botach+baarbaamsase";
+    $.ajax({
+        type: 'GET',
+        url: action,
+        data: { 
+        		vwitem: "baarbaamsase",
+        		vwquantity: "3",
+        		sectionId: "ysco.cart"
+        		//autodone: "http://www.botachtactical.com/vlbcmgumochh.html"
+        		//vwcatalog: "botach"
+        	  },
+        dataType: 'json',
+        success: function( data, textStatus, jqXHR ) {
+        	console.log ( "success" );
+        	console.log ( "data: " + data );
+			console.log ( "textStatus: " + textStatus );
+			console.log ( "jqXHR: " + jqXHR );
+        },
+        error: function( jqXHR, textStatus, errorThrown ){
+        	console.log ( "error" );
+			console.log ( "jqXHR: " + jqXHR );
+			console.log ( "textStatus: " + textStatus );
+			console.log ( "errorThrown: " + errorThrown );
+        }
+  });*/
